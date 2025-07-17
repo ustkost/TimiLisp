19f3b03163c0025db9e86547d3a15654f16a3a8d
Konstantin Ustiuzhanin <90912694+ustkost@users.noreply.github.com>
2025-07-17 23:47:23 +0300

    merg 2

--- src/Repl.hs
+++ src/Repl.hs
@@ -1,25 +1,13 @@
 module Repl (repl) where
 import Lexer (lexer)
 import Parser (Expr(..), parser)
-import Eval (eval, State)
+import Eval (eval, State, prettyPrint)
 
 -- printExprs :: [Expr] -> IO ()
 -- printExprs [] = ()
 -- printExprs (x:xs) = do 
 --   print x
 --   printExprs xs
-
-prettyPrint :: Expr -> String
-prettyPrint (Atom x) = x
-prettyPrint (Number x) = show x
-prettyPrint (StringLit x) = x
-prettyPrint (Error reason) = "Error: " ++ reason
-prettyPrint (List elems) = "(" ++ go elems ++ ")"
-  where
-    go :: [Expr] -> String
-    go [] = ""
-    go [x] = prettyPrint x
-    go (x:xs) = prettyPrint x ++ " " ++ go xs
 
 repl :: State -> IO ()
 repl env = do
@@ -28,7 +16,7 @@ repl env = do
   let 
     tokens = lexer input
     expr = parser tokens [] !! 0
-    (res, newEnv) = eval expr env
+  (res, newEnv) <- eval expr env
   putStrLn (prettyPrint res)
   repl newEnv
 

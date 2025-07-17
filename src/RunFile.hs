c405fa1f6ff26b955b50c4a82b95b75bf4d1834b
ustkost <q2zoff.jr@gmail.com>
2025-07-18 00:15:34 +0300

    read from file ADVANCED

--- src/RunFile.hs
+++ src/RunFile.hs
@@ -1,22 +1,39 @@
 module RunFile where
 
+import Data.List.Split (splitOn)
+
 import Lexer (lexer)
 import Parser (parser)
 import Eval (eval, State)
-import Parser (Expr)
+import Parser (Expr(Error))
 
 runFile :: FilePath -> IO ()
 runFile name = do
   content <- readFile name
   let
-    tokens = lexer content 
+    lines = splitOn "\n" content
+  execute lines ([], [])
+    where
+      execute :: [String] -> State -> IO ()
+      execute [] state = return ()
+      execute (x:xs) state = do
+        newState <- runLine x state
+        execute xs newState
+
+runLine :: String -> State -> IO (State)
+runLine line state = do
+  let
+    tokens = lexer line 
     exprs = parser tokens []
 
-  _ <- run exprs ([], [])
-  return ()
+  run exprs state
     where
       run :: [Expr] -> State -> IO (State)
       run [] state = return state
       run (x:xs) state = do
-        (_, newState) <- eval x state
-        run xs newState
+        (res, newState) <- eval x state
+        case res of
+          Error reason -> do
+            print reason
+            return state
+          _ -> run xs newState
